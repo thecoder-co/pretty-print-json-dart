e25167450e8554f4b0452b8bde93b94b15d847e6
thecoder-co <thecoder.co@gmail.com>
2023-02-11 13:06:15 +0100

    prettyJson

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
 ## 1.0.0
 
 - Initial version.
+
+## 1.0.1
+
+- Added ```prettyJson``` to return a prettified json string
+- Added ```toPrettyJson``` extension to return a prettified json string
--- lib/src/pretty_print_json_base.dart
+++ lib/src/pretty_print_json_base.dart
@@ -6,11 +6,20 @@ const encoder = JsonEncoder.withIndent('  ');
 
 /// Accepts json string or object and prints it in a prettified format.
 void prettyPrintJson(Object input) {
+  log(prettyJson(input));
+}
+
+/// Accepts json string or object and returns it in a prettified format.
+String prettyJson(Object input) {
   if (input is! String) {
     input = json.encode(input);
   }
 
   var object = decoder.convert(input);
   var prettyString = encoder.convert(object);
-  log(prettyString);
+  return prettyString;
+}
+
+extension PrettyPrintJsonExtension on String {
+  String get toPrettyJson => prettyJson(this);
 }
